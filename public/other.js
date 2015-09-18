b731290f876413eef210fac98ccae2b5efbb5fc9
Halvard Skogsrud <halvard@skogsrud.com>
2015-09-19 00:26:09 +1000

    Use handlebars on client side too

--- public/other.js
+++ public/other.js
@@ -1,6 +1,34 @@
 'use strict';
 
 module.exports = () => {
-  let page = document.querySelector('.other.page');
-  page.style.display = '';
+  let handlebars = require('handlebars');
+
+  var request = new XMLHttpRequest();
+  request.open('GET', '/books.json', true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 300) {
+      var books = JSON.parse(request.responseText);
+      console.log(books);
+      renderBooks(books);
+    } else {
+      console.log(`Error: Code: ${request.status} Message: ${request.responseText}`);
+    }
+  };
+
+  request.onerror = function(event) {
+    console.log(`Request error: ${event}`);
+  };
+
+  request.send();
+
+  function renderBooks(books) {
+    let booksTemplateScript = document.querySelector('#books-template').innerHTML;
+    let booksTemplate = handlebars.compile(booksTemplateScript);
+    let temp = booksTemplate(books);
+    console.log(temp);
+    document.querySelector('#books-list').innerHTML = temp;
+    document.querySelector('.other.page').style.display = '';
+  }
+
 }
